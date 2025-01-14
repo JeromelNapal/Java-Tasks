fccd536f37c814be01d79dcd19a567497f90f360
JeromelNapal <jeromelnapal10@gmail.com>
2025-01-15 03:46:07 +0800

    Create Primitives.java

---
+++ Task-1-1-2/Primitives.java
@@ -0,0 +1,17 @@
+  public class Primitives {
+    public static void main(String[] args) {
+        byte zero = 0;
+        short three = 3;
+        int one = 1;
+        float twoPointZero = 2.0f;
+        char h = 'H';
+        char w = 'w';
+        char r = 'r';
+        char d = 'd';
+        boolean isTrue = true;
+
+        String output = "" + h + three + one + one + zero + " " + w + zero + r + one + d + " " + twoPointZero + " " + isTrue;
+
+        System.out.println(output);
+    }
+}
